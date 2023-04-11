813ec29289ed030102185d15026d6a2d07e4dd52
Your Name <saifi.mujeeb2@gmail.com>
2023-04-12 00:24:32 +0530

    Added: player winning position

--- src/SnackAndLadder/SnackAndLadder.java
+++ src/SnackAndLadder/SnackAndLadder.java
@@ -10,24 +10,30 @@ public class SnackAndLadder {
     }
     public static void main(String[] args) {
         int player1 = 0;
+        int dice = 0;
+        int cond = 0;
+        int temp = 0;
         System.out.println(player1);
-        int dice = (int) dice();
-        System.out.println(dice);
-        int cond = condition();
-        System.out.println(cond);
-        switch (cond) {
-            case 0:
-                System.out.println("No Play");
+        while (player1 != 100) {
+            dice = (int) dice();
+            cond = condition();
+            switch (cond) {
+                case 0:
+                    temp = 0;
+                    break;
+                case 1:
+                    temp = player1 + dice;
+                    break;
+                case 2:
+                    temp = player1 - dice;
+                    break;
+            }
+            player1 = player1 + temp;
+            if (player1 < 0)
                 player1 = 0;
-                break;
-            case 1:
-                System.out.println("Ladder");
-                player1 = player1 + dice;
-                break;
-            case 2:
-                System.out.println("Snack");
-                player1 = player1 - dice;
-                break;
         }
+        System.out.println(player1);
+        if (player1 == 100)
+            System.out.println("You Won");
     }
 }
