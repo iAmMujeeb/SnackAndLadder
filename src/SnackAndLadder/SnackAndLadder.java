de6e53dbbaf7284af2ddb99cc12d8dc87ac7c2fa
Your Name <saifi.mujeeb2@gmail.com>
2023-04-11 23:55:24 +0530

    Added: Starting position of player

--- src/SnackAndLadder/SnackAndLadder.java
+++ src/SnackAndLadder/SnackAndLadder.java
@@ -2,7 +2,8 @@ package SnackAndLadder;
 
 public class SnackAndLadder {
     public static void main(String[] args) {
-        int player1=0;
+        int player1 = 0;
         System.out.println(player1);
+        double random = Math.floor(Math.random() * (6 - 1 + 1) + 1);
     }
 }
